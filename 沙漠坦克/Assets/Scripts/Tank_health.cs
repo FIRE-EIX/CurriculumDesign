dfc054f6d6a89d18c69d97dc543f847c6fb7f81d
tktc <1145855291@qq.com>
2021-06-21 09:59:16 +0800

    第三次提交，实现基本功能

--- 沙漠坦克/Assets/Scripts/Tank_health.cs
+++ 沙漠坦克/Assets/Scripts/Tank_health.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Tank_health : MonoBehaviour {
 
     public int hp = 100;
     public GameObject tankExplosion;
+    //public AudioClip tankExplosionAudio;
+    public Slider hpSlider;
+    private int hpTotal;
     // Use this for initialization
     void Start()
     {
-
+        hpTotal = hp;
     }
 
     // Update is called once per frame
@@ -22,8 +25,10 @@ public class Tank_health : MonoBehaviour {
         if (hp <= 0)
             return;
         hp -= Random.Range(10, 20);
+        hpSlider.value = (float)hp / hpTotal;
         if (hp <= 0)
         {
+            //AudioSource.PlayClipAtPoint(tankExplosionAudio,transform.position);
             GameObject.Instantiate(tankExplosion, transform.position + Vector3.up, transform.rotation);
             GameObject.Destroy(this.gameObject);
         }
